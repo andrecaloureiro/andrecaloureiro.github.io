4c10f03da93ca9cb6469db532bd0eea54830f26c
andrecaloureiro <andre.nox@gmail.com>
2025-03-14 16:37:03 -0300

    config.js apontando para Render

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 // config.js
 
 //para teste local
-const BACKEND_URL = "http://localhost:3000";
+//const BACKEND_URL = "http://localhost:3000";
 
 // paraprodução: 
-// const BACKEND_URL = https://lms-backendv1.onrender.com em produção
+const BACKEND_URL = "https://lms-backendv1.onrender.com";
 
 export default BACKEND_URL;
